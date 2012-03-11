e44c35e255a7c216dcd616e3dd44d40845c867fc
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-03-11 06:56:08 -0700

    Ajout du FakerBundle pour populer la database facilement

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Babbel\UserBundle\BabbelUserBundle(),
+            new Bazinga\Bundle\FakerBundle\BazingaFakerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
